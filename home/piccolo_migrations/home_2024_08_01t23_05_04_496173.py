80e254028e1bf8ce4084e657f92c773b8e1934f1
skelmis <ethanmkh2@gmail.com>
2024-08-01 23:05:36 +1200

    feat: add vulnerabilities table

---
+++ home/piccolo_migrations/home_2024_08_01t23_05_04_496173.py
@@ -0,0 +1,193 @@
+from piccolo.apps.migrations.auto.migration_manager import MigrationManager
+from piccolo.columns.base import OnDelete
+from piccolo.columns.base import OnUpdate
+from piccolo.columns.column_types import ForeignKey
+from piccolo.columns.column_types import Text
+from piccolo.columns.column_types import UUID
+from piccolo.columns.defaults.uuid import UUID4
+from piccolo.columns.indexes import IndexMethod
+from piccolo.table import Table
+
+
+class Project(Table, tablename="project", schema=None):
+    id = UUID(
+        default=UUID4(),
+        null=False,
+        primary_key=True,
+        unique=False,
+        index=True,
+        index_method=IndexMethod.btree,
+        choices=None,
+        db_column_name=None,
+        secret=False,
+    )
+
+
+ID = "2024-08-01T23:05:04:496173"
+VERSION = "1.13.1"
+DESCRIPTION = ""
+
+
+async def forwards():
+    manager = MigrationManager(
+        migration_id=ID, app_name="home", description=DESCRIPTION
+    )
+
+    manager.add_table(
+        class_name="Vulnerability",
+        tablename="vulnerability",
+        schema=None,
+        columns=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="id",
+        db_column_name="id",
+        column_class_name="UUID",
+        column_class=UUID,
+        params={
+            "default": UUID4(),
+            "null": False,
+            "primary_key": True,
+            "unique": False,
+            "index": True,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="project",
+        db_column_name="project",
+        column_class_name="ForeignKey",
+        column_class=ForeignKey,
+        params={
+            "references": Project,
+            "on_delete": OnDelete.cascade,
+            "on_update": OnUpdate.cascade,
+            "target_column": None,
+            "null": True,
+            "primary_key": False,
+            "unique": True,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="title",
+        db_column_name="title",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="description",
+        db_column_name="description",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="code_file",
+        db_column_name="code_file",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="code_line",
+        db_column_name="code_line",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    manager.add_column(
+        table_class_name="Vulnerability",
+        tablename="vulnerability",
+        column_name="code_context",
+        db_column_name="code_context",
+        column_class_name="Text",
+        column_class=Text,
+        params={
+            "default": "",
+            "null": False,
+            "primary_key": False,
+            "unique": False,
+            "index": False,
+            "index_method": IndexMethod.btree,
+            "choices": None,
+            "db_column_name": None,
+            "secret": False,
+        },
+        schema=None,
+    )
+
+    return manager
--- home/tables.py
+++ home/tables.py
@@ -2,9 +2,32 @@ import uuid
 
 from piccolo.apps.user.tables import BaseUser
 from piccolo.table import Table
-from piccolo.columns import UUID, ForeignKey
+from piccolo.columns import UUID, ForeignKey, Text
 
 
 class Project(Table):
     id = UUID(primary_key=True, default=uuid.uuid4, index=True)
     owner = ForeignKey(BaseUser, unique=True, help_text="Who owns this project")
+
+
+class Vulnerability(Table):
+    id = UUID(primary_key=True, default=uuid.uuid4, index=True)
+    project = ForeignKey(
+        Project,
+        unique=True,
+        help_text="The project this vulnerability exists on",
+    )
+    title = Text()
+    description = Text()
+    code_file = Text(
+        default="",
+        help_text="A path to the code at fault",
+    )
+    code_line = Text(
+        default="",
+        help_text="The line of code which threw this error",
+    )
+    code_context = Text(
+        default="",
+        help_text="The code related to this issue",
+    )

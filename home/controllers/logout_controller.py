fbcd43085524aa20e7429f2d1f6cbf6622f96893
skelmis <ethanmkh2@gmail.com>
2024-08-04 21:09:11 +1200

    feat: move sign out to within the dashboard

--- home/controllers/logout_controller.py
+++ home/controllers/logout_controller.py
@@ -1,34 +1,21 @@
-import os
-import typing as t
-
-from jinja2 import Environment, FileSystemLoader
-from litestar import Controller, Request, post, get
-from piccolo.apps.user.tables import BaseUser
-from piccolo_api.session_auth.endpoints import LOGOUT_TEMPLATE_PATH
+from litestar import Controller, Request, post, get, MediaType
+from litestar.response import Redirect, Template
 from piccolo_api.session_auth.tables import SessionsBase
 from piccolo_api.shared.auth.styles import Styles
-from starlette.exceptions import HTTPException
-from starlette.responses import HTMLResponse, RedirectResponse
 from starlette.status import HTTP_303_SEE_OTHER
 
-directory, filename = os.path.split(LOGOUT_TEMPLATE_PATH)
-environment = Environment(loader=FileSystemLoader(directory), autoescape=True)
-login_template = environment.get_template(filename)
 
-
-# Taken from the underlying Piccolo class and modified to work with Litestar
 class LogoutController(Controller):
     path = "/logout"
-    _auth_table = BaseUser
     _session_table = SessionsBase
     _redirect_to = "/"
     _cookie_name = "id"
-    _logout_template = login_template
     _styles = Styles()
 
     def _render_template(
-        self, request: Request, template_context: t.Dict[str, t.Any] = {}
-    ) -> HTMLResponse:
+        self,
+        request: Request,
+    ) -> Template:
         # If CSRF middleware is present, we have to include a form field with
         # the CSRF token. It only works if CSRFMiddleware has
         # allow_form_param=True, otherwise it only looks for the token in the
@@ -36,32 +23,31 @@ class LogoutController(Controller):
         csrftoken = request.scope.get("csrftoken")
         csrf_cookie_name = request.scope.get("csrf_cookie_name")
 
-        return HTMLResponse(
-            self._logout_template.render(
-                csrftoken=csrftoken,
-                csrf_cookie_name=csrf_cookie_name,
-                request=request,
-                styles=self._styles,
-                **template_context,
-            )
+        return Template(
+            "logout.jinja",
+            context={
+                "csrftoken": csrftoken,
+                "csrf_cookie_name": csrf_cookie_name,
+                "request": request,
+                "styles": self._styles,
+            },
+            media_type=MediaType.HTML,
         )
 
     @get(include_in_schema=False, name="signout")
-    async def get(self, request: Request) -> HTMLResponse:
+    async def get(self, request: Request) -> Template:
         return self._render_template(request)
 
     @post(tags=["Auth"])
-    async def post(self, request: Request) -> RedirectResponse:
+    async def post(self, request: Request) -> Redirect:
         cookie = request.cookies.get(self._cookie_name, None)
         if not cookie:
-            raise HTTPException(
-                status_code=401, detail="The session cookie wasn't found."
-            )
+            # Meh this is fine, just redirect it to home
+            return Redirect("/")
+
         await self._session_table.remove_session(token=cookie)
 
-        response: RedirectResponse = RedirectResponse(
-            url=self._redirect_to, status_code=HTTP_303_SEE_OTHER
-        )
+        response: Redirect = Redirect(self._redirect_to, status_code=HTTP_303_SEE_OTHER)
 
         response.set_cookie(self._cookie_name, "", max_age=0)
         return response

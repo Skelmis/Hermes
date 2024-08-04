b148e3c42170a802f3a62f60cef665abe3189d08
skelmis <ethanmkh2@gmail.com>
2024-08-04 21:02:48 +1200

    feat: add log in and out to sidebar

--- home/controllers/logout_controller.py
+++ home/controllers/logout_controller.py
@@ -46,7 +46,7 @@ class LogoutController(Controller):
             )
         )
 
-    @get(include_in_schema=False)
+    @get(include_in_schema=False, name="signout")
     async def get(self, request: Request) -> HTMLResponse:
         return self._render_template(request)
 

cf28771bb77c342e40ba305009de3b9903e2290a
skelmis <skelmis.craft@gmail.com>
2025-03-17 22:38:06 +1300

    feat: cloudflare CSP requirements for web analytics

--- home/util/get_csp.py
+++ home/util/get_csp.py
@@ -4,8 +4,8 @@ import secrets
 def get_csp() -> tuple[str, str]:
     nonce = secrets.token_urlsafe(16)
     text = (
-        "default-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'none'; script-src 'nonce-{}' "
-        "'strict-dynamic'; style-src 'nonce-{}' 'strict-dynamic'; require-trusted-types-for 'script'"
+        "default-src 'none'; frame-ancestors 'none'; object-src 'none'; base-uri 'none'; script-src static.cloudflareinsights.com 'nonce-{}' "
+        "'strict-dynamic'; style-src 'nonce-{}' 'strict-dynamic'; require-trusted-types-for 'script'; connect-src cloudflareinsights.com"
     )
     text = text.format(nonce, nonce)
     return text, nonce

c4229b61354de64134a463d1a9cfcd8e88f63100
skelmis <skelmis.craft@gmail.com>
2025-04-01 19:22:10 +1300

    feat: notify on looping of next vuln
    Closes #22

--- home/controllers/projects_controller.py
+++ home/controllers/projects_controller.py
@@ -111,6 +111,11 @@ class ProjectsController(Controller):
             if vuln.id == current_vulnerability.id:
                 if len(vulns) - 1 == i:
                     # Its last vuln so loop around
+                    alert(
+                        request,
+                        "FYI, you've looped back to the"
+                        " start of vulnerabilities for this scan.",
+                    )
                     return vulns[0].id
                 else:
                     return vulns[i + 1].id
